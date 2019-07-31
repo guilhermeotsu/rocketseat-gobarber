09d85e1d8cf73059f813350e80c797a191b72f4f
Guilherme Otsu <guiotsu@gmail.com>
2019-07-30 23:55:15 -0300

    validação de dados de entrada do usuário

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,11 +1,24 @@
 // controle de autenticação de usuario
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string().required(),
+        });
+
+        // validando se o req.body está de acordo com o schema definido
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const { email, password } = req.body;
 
         // verificando se o email que usario está tentado acessar existe
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,26 @@
+import * as Yup from 'yup'; // nao da para importar igual as outras, yup nao tem export default
 import User from '../models/User';
 
 class UserController {
     // método de  criação
     async store(req, res) {
+        // validações de dados de entrada
+        const schema = Yup.object().shape({
+            // definindo o formato do objeto que é passado no body
+            name: Yup.string().required(),
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string()
+                .required()
+                .min(6),
+        });
+
+        // validando se o req.body está de acordo com o schema definido
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const userExist = await User.findOne({
             where: { email: req.body.email },
         });
@@ -22,6 +40,30 @@ class UserController {
 
     // método de update, usuario fazer modificação de seus dados cadastrais
     async update(req, res) {
+        // validações de dados de entrada
+        const schema = Yup.object().shape({
+            // definindo o formato do objeto que é passado no body
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string()
+                .min(6)
+                // when é uma validação condicional
+                .when('oldPassword', (oldPassword, field) =>
+                    // field se refere ao password
+                    oldPassword ? field.required() : field
+                ),
+            // quando o usuario quiser alterar a senha vai ter que ter uma confirmação da senha
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            ),
+        });
+
+        // validando se o req.body está de acordo com o schema definido
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         // console.log(req.userId);  pegar a variavel que vai inserida dentro do req pelo middleware auth
         const { email, oldPassword } = req.body;
 
