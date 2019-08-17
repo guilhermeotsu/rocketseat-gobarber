31a346b9e7cce9f653134fcaf7767f1b5439b177
Guilherme Otsu <guiotsu@gmail.com>
2019-08-16 23:36:46 -0300

    Tratamento de exceções com sentry

--- src/app.js
+++ src/app.js
@@ -1,24 +1,32 @@
 // usar a sintaxe de import só é possivel após a instalação do sucrase
 import express from 'express';
 import path from 'path';
-// importando as rotas de outro arquivo
-import routes from './routes';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import routes from './routes'; // importando as rotas de outro arquivo
+import sentryConfig from './config/sentry';
 
 // importando o arquivo que faz a conexao com o banco de dados e carrega os models
 import './database';
 
-// isntanciando a aplicação
+// instanciando a aplicação
 class App {
     constructor() {
         // apenas o server vai ser exportado da aplicação
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
+        this.exceptionHandler();
     }
 
     // métodos:
     middlewares() {
+        this.server.use(Sentry.Handlers.requestHandler());
+
         // preparar para que nossa aplicação possa receber arquivos em json
         this.server.use(express.json());
 
@@ -31,6 +39,15 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandler() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON();
+
+            return res.status(500).json(errors);
+        })
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dns: 'https://bb62a32922bf4211a75180d14112d992@sentry.io/1533245',
+};
